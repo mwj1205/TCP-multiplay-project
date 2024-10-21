231856657137bee3d39feb9789169afc4951b269
Minu Han <mwj1205@naver.com>
2024-10-21 17:58:07 +0900

    이벤트 구분, 바이트 배열 분해

---
+++ src/events/onData.js
@@ -0,0 +1,23 @@
+import { config } from '../config/config.js';
+
+export const onData = (socket) => (data) => {
+  socket.buffer = Buffer.concat([socket.buffer, data]);
+
+  const totalHeaderLength = config.packet.totalLength + config.packet.typeLength;
+
+  while (socket.buffer.length >= totalHeaderLength) {
+    const length = socket.buffer.readUInt32BE(0);
+    const packetType = socket.buffer.readUInt8(config.packet.totalLength);
+
+    if (socket.buffer.length >= length) {
+      const packet = socket.buffer.subarray(totalHeaderLength, length);
+      socket.buffer = socket.buffer.subarray(length);
+
+      console.log(`length: ${length}, packetType: ${packetType}`);
+      console.log(`packet: ${packet}`);
+    } else {
+      // 아직 전체 패킷이 도착하지 않았음
+      break;
+    }
+  }
+};
